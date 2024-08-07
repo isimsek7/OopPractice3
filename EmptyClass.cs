de78ebb21a6c8843fb602df98da7808839970596
isimsek7 <isimsekmusic@gmail.com>
2024-08-07 22:02:47 +0300

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ClassObj
+{
+	public class Kitap
+	// Icerisinde alanlar, yapilandiricilar ve metodlar barindiran siniflardir.
+	{
+	//Sinif sablonlarinin olusumunu saglayan gerek parametreli gerek parametresiz metod ve degisken arasi ozelliktir.
+		public Kitap()
+		{
+			DateTime tarih = DateTime.Now;
+			_kayitTarihi = tarih.ToLongDateString();
+		}
+		public Kitap(string ad, string soyad, string kitapAdi, int sayfaSayisi, string yayinEviAdi)
+		{
+			_ad = ad;
+			_soyAd = soyad;
+			_kayitTarihi = DateTime.Now.ToLongDateString();
+			_sayfaSayisi = sayfaSayisi;
+			_yayinEviAdi = yayinEviAdi;
+			_kitapAdi = kitapAdi;
+			Console.WriteLine(ad + " " + soyad + "'dan " + kitapAdi + ". Sayfa sayisi= " + sayfaSayisi + "," + yayinEviAdi);
+
+		}
+	//Sinifin nesnelerinin sahip olabilecegi degiskenlerdir.
+		public string _ad { get; set; }
+		public string _soyAd { get; set; }
+		public string _kitapAdi { get; set; }
+		public string _kayitTarihi { get; set; }
+		public int _sayfaSayisi { get; set; }
+		public string _yayinEviAdi { get; set; }
+
+		public void Ozet()
+		{
+            Console.WriteLine(_ad + " " + _soyAd + "'dan " + _kitapAdi + ". Sayfa sayisi=" + _sayfaSayisi + "," + _yayinEviAdi);
+        }
+	}
+	
+	
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassObj;
+string isim = "Mihail";
+string soyIsim = "Bulgakov";
+string kitapIsmi = "Usta ile Margarita";
+int sayfa = 608;
+string yayinEviAdi = "Ithaki Yayinlari";
+//New anahtar kelimesi, siniftan instans almamizi saglar.
+Kitap kitap1 = new Kitap(isim, soyIsim, kitapIsmi, sayfa, yayinEviAdi);
+
+Kitap kitap2 = new Kitap();
+kitap2._ad = "Oscar";
+kitap2._soyAd = "Wilde";
+kitap2._kitapAdi = "Dorian Gray'in Portresi";
+kitap2._sayfaSayisi = 272;
+kitap2._yayinEviAdi = "Can Yayinevi";
+kitap2.Ozet();
